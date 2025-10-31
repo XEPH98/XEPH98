9df1ed5d728d0a76ef439b445e19d704f18130c2
XEPH98 <135947803+XEPH98@users.noreply.github.com>
2025-10-31 23:23:37 +0800

    Update 251031.py

--- 251031.py
+++ 251031.py
@@ -1,2 +1,11 @@
-class Contact:
-    all_contacts: List["Contact"] = []
+from dataclasses import dataclass
+
+@dataclass
+class Member:
+    section: str
+    area_in2: float
+
+
+m1 = Member("3X3X1/2", 3.20)
+
+print(m1)
