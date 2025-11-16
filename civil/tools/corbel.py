9f53c7bc04e76f53d9956db201276d91ade92bce
XEPH98 <135947803+XEPH98@users.noreply.github.com>
2025-11-16 16:25:37 +0800

    Update corbel.py

--- civil/tools/corbel.py
+++ civil/tools/corbel.py
@@ -1,6 +1,25 @@
 from dataclasses import dataclass
 from typing import List
 
+@dataclass
+class anchor_bolt:
+    """Origin always at bottom left of plate.
+    Args:
+        x (float): inch, horizontal distance
+        y (float): inch, vertical distance
+    """
+    x: float
+    y: float
+
+@dataclass
+class group_anchor:
+    anchors: List[anchor_bolt]
+
+a1 = anchor_bolt(2,2)
+a2 = anchor_bolt(4,2)
+a3 = anchor_bolt(2,4)
+a4 = anchor_bolt(4,4)
+
 class anchor:
     def __init__(self, dia, futa, n_anchor, cc_dist, edge_dist, hef):
         """_summary_
@@ -13,7 +32,7 @@ class anchor:
             edge_dist (float): _description_
             hef (float): _description_
         """
-        self.dia:float = dia
+        self.dia = dia
         self.futa = futa
         self.n_anchor = n_anchor
         self.cc_dist = cc_dist
@@ -25,7 +44,7 @@ class anchor:
         """Calculation of A_nco per ACI 318M 19, Figure R17.6.2.1"""
         return round(2*1.5*self.hef*2*1.5*self.hef,2)
     def proj_area_group_anchor(self) -> float:
-        if 
+        pass
     def strength_breakout(self):
-        if self.n_anchor = 1:
-            nominal_cb = 
+        if self.n_anchor == 1:
+            nominal_cb = 1
